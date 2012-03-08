084ce558ff32265faf6b17df1f7568f0772b6999
Rimian Perkins <hello@rimian.com.au>
2012-03-08 15:05:50 +1100

    refactor using add event instead of inititalizing and iterating through models manually.

--- js/pizza.js
+++ js/pizza.js
@@ -19,13 +19,7 @@
     localStorage: new Store('pizza_selected_toppings')
   });
 
-  window.pizzaMenu = new PizzaMenuCollection([
-    {title: 'Peperoni'},
-    {title: 'Anchovies'},
-    {title: 'Olives'},
-    {title: 'Capscicum'},
-    {title: 'Bocconcini'}
-  ]);
+  window.pizzaMenu = new PizzaMenuCollection();
   
   $(document).ready(function() {
     
@@ -60,46 +54,27 @@
       initialize: function() {
         console.log('App View : function: initialize');
         
+        pizzaMenu.bind('add', this.addMenuIngredient, this);
         
-        // Ingredients.bind('add',   this.addOne, this);
-        // Ingredients.bind('reset', this.addAll, this);
-        // pizzaMenu.bind('change', this.render);
-        
-        // console.log('App View : fetch pizza menu');
-        // pizzaMenu.fetch();
-        
-        // This should be bound to an event?
-        this.render();
+        pizzaMenu.add([
+          {title: 'Peperoni'},
+          {title: 'Anchovies'},
+          {title: 'Olives'},
+          {title: 'Capscicum'},
+          {title: 'Bocconcini'}
+        ]);
       },
       
       render: function() {
         console.log('App View : render');
-        
-        // This isn't right... there should be some iterator thing.
-        for(var i = 0; i < pizzaMenu.models.length; i++) {
-          console.log('App View : render : pizza menu', pizzaMenu.models[i]);
-          model = pizzaMenu.models[i];
-          this.addOne(model);
-        }
-        
-        
-      
-        
-        // $("#pizza_menu").append('');
       },
       
-      addOne: function(ingredient) {
-        console.log('App View : function: addOne', ingredient);
+      addMenuIngredient: function(ingredient) {
+        console.log('App View : render an ingredient view for pizza menu.', ingredient);
         var view = new IngredientView({model: ingredient});
         $("#ingredients_pizza").append(view.render().el);
       },
 
-      addAll: function() {
-        console.log('App View : function: addAll');
-        
-      }
-      
-      
     });
     
     window.app = new AppView();
